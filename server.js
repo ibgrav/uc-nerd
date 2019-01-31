0f20f7e410fdb1b53cfafa78382262c22cd23a3f
Isaac Graves <igraves@hbs.edu>
2019-01-30 21:50:37 -0500

    .

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/s3-all', function (req, res, next) {
     var params = {
         Bucket: S3_BUCKET,
         Delimiter: '',
-        Prefix: 's/5469b2f5b4292d22522e84e0/ms.files'
+        Prefix: ''
     }
 
     s3.listObjects(params, function (err, data) {
@@ -63,8 +63,7 @@ app.get('/s3-all', function (req, res, next) {
         console.log(data);
         res.send('<style>body {font-size: 20px;background-color:rgb(125,175,150); color:rgb(240,240,240);}</style><div style="width:100%; margin: auto; text-align: center"><br/><br/><br/><br/>' +
         'You have successfull uploaded the podcast<br/><br/><br/><br/><div style="text-align: left;font-size:14px;"></div><br/><br/><br/><br/>' +
-        'See if it works: <br/><br/>' + JSON.stringify(data) + '<br/><br/>'+
-        'Your browser does not support the audio element.</audio><br/><br/><a href="https://undercovercast.com/admin2.html">Make it live</a></div>');
+        'See if it works: <br/><br/>' + JSON.stringify(data) + '<br/><br/><br/><br/></div>');
     });
 });
 
